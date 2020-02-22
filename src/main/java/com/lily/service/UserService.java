2b63c96fa4b595ce8702c88e1cde9a80d91854a0
liling963@emao.com <liling963@emao.com>
2020-02-22 20:14:41 +0800

    springboot整合redis

--- src/main/java/com/lily/service/UserService.java
+++ src/main/java/com/lily/service/UserService.java
@@ -1,11 +1,12 @@
 package com.lily.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.lily.pojo.User;
 
 import java.util.List;
 
 public interface UserService {
 
-    List<User> findAll();
+    List<User> findAll() throws JsonProcessingException;
 
 }
--- src/main/java/com/lily/service/impl/UserServiceImpl.java
+++ src/main/java/com/lily/service/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.lily.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lily.mapper.UserMapper;
 import com.lily.pojo.User;
 import com.lily.service.UserService;
+import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,8 +25,25 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private RedisTemplate redisTemplate;
+
     @Override
-    public List<User> findAll() {
-        return userMapper.findAll();
+    public List<User> findAll() throws JsonProcessingException {
+        //从redis缓存读取
+        String users = (String) redisTemplate.boundValueOps("user:findAll").get();
+        ObjectMapper objectMapper = new ObjectMapper();
+        //判断缓存是否存在
+        if (StringUtil.isNullOrEmpty(users)){
+            System.out.println("UserServiceImpl.findAll" + "===数据库读取");
+            //不存在, 从数据库读取
+            List<User> all = userMapper.findAll();
+            //缓存到redis中
+            redisTemplate.boundValueOps("user:findAll").set(objectMapper.writeValueAsString(all));
+            return all;
+        }
+        //存在, 直接返回缓存数据
+        System.out.println("UserServiceImpl.findAll" + "===缓存读取");
+        return objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
 }
